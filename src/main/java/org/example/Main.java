f489987767d71a6470474ec60f2297eb7100969d
Aditya Mishra <im.aditya707@gmail.com>
2025-06-07 13:00:24 +0530

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+@Test(groups = "Verification")
+public class Main{
+
+    @Test(priority = 2,groups = "Regression")
+    public void test1()
+    {
+        System.out.println("Test one-Regression");
+    }
+    @Test(priority = 1,groups = {"Smoke","Regression"})
+    public void test2()
+    {
+        System.out.println("Test two-Smoke and Regression");
+    }
+    @Test(priority = 3,groups = "Regression")
+    public void test3()
+    {
+        System.out.println("Test three - Regression");
+    }
+    @BeforeTest
+    public void beforeTest4()
+    {
+        System.out.println("Before test");
+    }
+    @BeforeTest
+    public void beforeTest3()
+    {
+        System.out.println("Before test");
+    }
+
+
+
+}
