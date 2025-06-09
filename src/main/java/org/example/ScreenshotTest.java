1cb04533f1485fdbce467f8f06fcaa77195bb85c
Aditya Mishra <im.aditya707@gmail.com>
2025-06-09 21:34:10 +0530

    Testing data Prover and passing only usernam and clicking sign in

--- src/main/java/org/example/ScreenshotTest.java
+++ src/main/java/org/example/ScreenshotTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -14,11 +15,13 @@ import org.testng.annotations.Test;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Calendar;
+import java.util.Date;
 
 public class ScreenshotTest{
     WebDriver driver=new ChromeDriver();
     WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
-    @BeforeMethod
+    @BeforeMethod(groups = "Smoke")
     public void screenShot()
     {
         WebDriverManager.chromedriver().setup();
@@ -26,13 +29,18 @@ public class ScreenshotTest{
         driver.manage().window().maximize();
         driver.get("https://www.ebay.com/");
     }
-    @Test(description = "Taking screen shot test")
+    @Test(description = "Taking screen shot test", groups = "Smoke")
     public void test() throws IOException {
         File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         WebElement element=driver.findElement(By.xpath("//li[@class='vl-flyout-nav__js-tab']//child::a[text()='Electronics']"));
         wait.until(ExpectedConditions.elementToBeClickable(element)).click();
-        FileUtils.copyFile(screenshotFile,new File("ss1.jpg"));
+        Calendar calendar=Calendar.getInstance();
+        int minute=calendar.get(Calendar.MINUTE);
+        int sec=calendar.get(Calendar.SECOND);
+        FileUtils.copyFile(screenshotFile,new File(""+ minute +""+ sec +".jpg"));
+        throw new SkipException("Nhi karna");
     }
+
     @AfterMethod
     public void end()
     {
---
+++ src/main/java/org/example/TestingDataProve.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestingDataProve {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeMethod
+    public void preSetup()
+    {
+       WebDriverManager.chromedriver().setup();
+       driver=new ChromeDriver();
+       wait=new WebDriverWait(driver, Duration.ofSeconds(20));
+       driver.get("https://practicetestautomation.com/practice-test-login/");
+       driver.manage().window().maximize();
+
+    }
+
+
+
+    @Test(dataProvider = "dataProve")
+    public void test(String username)
+    {
+      WebElement element=driver.findElement(By.xpath("//input[@name='username']"));
+      WebElement element1=driver.findElement(By.id("submit"));
+      wait.until(ExpectedConditions.visibilityOf(element)).sendKeys(username);
+      wait.until(ExpectedConditions.visibilityOf(element1)).click();
+    }
+
+    @AfterMethod
+    public void postSetup()
+    {
+        driver.close();
+    }
+
+
+    @DataProvider
+    public Object[] dataProve()
+    {
+        return new Object[]{"Hellouser","User_1999","2000_User"};
+
+
+    }
+
+
+}
