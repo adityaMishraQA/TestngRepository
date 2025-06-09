07351e3c384a71c74ed31c75090f200c1c1ca03a
Aditya Mishra <im.aditya707@gmail.com>
2025-06-09 22:44:56 +0530

    Adding two or more number using data provider

---
+++ src/main/java/org/example/DataProviderSumOfNumber.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviderSumOfNumber {
+
+
+
+
+    @DataProvider
+    public Object[][] dataSum()
+    {
+        return new Object[][]{{2,3,3},{1,2,4},{3,2,5}};
+    }
+
+
+
+    @Test(dataProvider = "dataSum")
+    public void test1(int user, int pass, int passtwo)
+    {
+        System.out.println(user+pass+passtwo);
+    }
+}
