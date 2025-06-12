f2cd92dff76249c03e6c0d5ce15fab8aef3ab867
Aditya Mishra <im.aditya707@gmail.com>
2025-06-12 22:08:45 +0530

    adding parameter , screenshot ,dataprover and listners together

--- src/main/java/org/example/listner/DataProviderPractise.java
+++ src/main/java/org/example/listner/DataProviderPractise.java
@@ -2,64 +2,96 @@ package org.example.listner;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.SkipException;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Calendar;
 
 @Listeners(org.example.listner.DataProvieForm.class)
-public class DataProviderPractise {
+public class DataProviderPractise{
+
     WebDriver driver;
     WebDriverWait wait;
-    Actions actions;
+    Actions act;
 
-    @BeforeMethod
-    public void preSetUp()
+
+    @Parameters("browser")
+    @BeforeTest
+    public void preSetup(String browser)
     {
-        WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
-        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.get("https://store.steampowered.com/");
+        if(browser.equals("Chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver=new ChromeDriver();
+        }
+        else if(browser.equals("Firefox"))
+        {
+            WebDriverManager.firefoxdriver().setup();
+            driver=new FirefoxDriver();
+        }
+        else {
+            WebDriverManager.edgedriver().setup();
+            driver=new EdgeDriver();
+        }
+
+        driver.get("https://www.flipkart.com/");
         driver.manage().window().maximize();
-        actions=new Actions(driver);
+
+
     }
 
-    @Test(dataProvider = "dataPass",dataProviderClass = org.example.listner.DataProvieForm.class)
-    public void test(String data)
+    @Test(enabled = false,priority = 0,dataProvider = "DataProvide",dataProviderClass = org.example.listner.DataProvieForm.class)
+    public void test(String name)
     {
-        WebElement element=driver.findElement(By.xpath("//a[@class='gutter_item' and @href='https://store.steampowered.com/genre/Free%20to%20Play/?snr=1_4_4__125']"));
-        actions.scrollToElement(element);
-        actions.perform();
-        wait.until(ExpectedConditions.visibilityOf(element)).click();
-        WebElement element1=driver.findElement(By.id("store_nav_search_term"));
-        wait.until(ExpectedConditions.elementToBeClickable(element1)).click();
-        element1.sendKeys(""+data+"");
+        wait= new WebDriverWait(driver, Duration.ofSeconds(10));
+        act=new Actions(driver);
+        WebElement element=driver.findElement(By.xpath(""));
+        wait.until(ExpectedConditions.visibilityOf(element));
+        act.moveToElement(element);
+        act.perform();
 
     }
-    @Test
-    public void test2()
-    {
-        System.out.println("Test two going to start");
-        throw new SkipException("Skipping");
+    @Test(priority = 1,dataProvider = "DataProvide",dataProviderClass = org.example.listner.DataProvieForm.class)
+    public void test2(String name) throws IOException {
+        wait= new WebDriverWait(driver, Duration.ofSeconds(10));
+        act=new Actions(driver);
+
+        /*WebElement element1=driver.findElement(By.xpath("//a[@class='_1BJVlg _11MZbx']"));
+        element1.click();*/
+        WebElement element2=driver.findElement(By.xpath("//input[starts-with(@title,'S')]"));
+        wait.until(ExpectedConditions.elementToBeClickable(element2)).click();
+        element2.clear();
+        File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        Calendar calendar=Calendar.getInstance();
+        int min=calendar.get(Calendar.MINUTE);
+        int sec=calendar.get(Calendar.SECOND);
+        FileUtils.copyFile(screenshotFile,new File(".//Screenshots//"+min+"-"+sec+".jpg"));
+        element2.sendKeys(""+name+""+ Keys.ENTER);
+        System.out.println(name);
+        WebElement element3=driver.findElement(By.xpath("//span[text()='Yes']"));
+        wait.until(ExpectedConditions.elementToBeClickable(element3));
+        act.scrollToElement(element3);
+        act.perform();
+
+
+
+
+
     }
 
-    @AfterMethod
-    public void postSetUp()
+    @AfterTest
+    public void postSetup()
     {
         driver.close();
     }
-
-
-
-
 }
--- src/main/java/org/example/listner/DataProvieForm.java
+++ src/main/java/org/example/listner/DataProvieForm.java
@@ -1,20 +1,36 @@
 package org.example.listner;
 
+
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.annotations.DataProvider;
 
+import java.lang.reflect.Method;
+
 public class DataProvieForm implements ITestListener {
 
-    @DataProvider(name = "dataPass")
-    public Object[] data()
-    {
-        return new Object[]{"cs"};
+    @Override
+    public void onTestFailure(ITestResult result) {
+        ITestListener.super.onTestFailure(result);
+
+        System.out.println("Test  Failed");
     }
 
-    @Override
-    public void onTestSkipped(ITestResult result) {
-        ITestListener.super.onTestSkipped(result);
-        System.out.println("Test got skipped");
+    @DataProvider(name = "DataProvide")
+    public Object[] dataProvide(Method m)
+    {
+        if(m.getName().equals("test"))
+        {
+            return new Object[]{"Not possible"};
+        }
+        else {
+            return new Object[]{"Gym","Graphics Card","Lamborgini","Chakka"};
+        }
+
     }
+
+
+
 }
+
+
